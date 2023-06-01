28aac3e1f2751aa9e2301263f2c2c4197e2390da
FannyDFT <fannyd.erfurth@yahoo.fr>
2023-06-01 22:21:46 +0200

    footer ok responsive

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import insta from "../../../../public/assets/insta.png";
+import facebook from "../../../../public/assets/fb.png";
+import arrow from "../../../../public/assets/arrowLogo.png";
+import Link from "next/link";
+import Image from "next/image";
+
+function Footer() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 750);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div className="h-96 bg-terracota flex flex-col gap-3 justify-between items-center text-whiteText text-sm font-imprima p-4 sm:flex-row sm:h-48 sm:justify-between sm:text-l ">
+      <div className="flex flex-col gap-1 text-center">
+        <p>14 rue Colombet Solle</p>
+        <p>42 270 Saint-Just-Saint-Rambert</p>
+        <p> Contactez-moi</p>
+        <p>07 78 69 33 86</p>
+      </div>
+      <div className="flex flex-col text-center gap-4">
+        <div className="font-ibarra text-lg ">INFORMATIONS</div>
+        <div className="flex flex-col gap-1">
+          <p>Mardi: 9h30 - 19h00</p>
+          <p>Mercredi: 9h00 - 13h00</p>
+          <p>Jeudi: 10h30 - 20h00</p>
+          <p>Vendredi -Samedi: 9h30 - 17h00</p>
+        </div>
+      </div>
+      <div className=" flex flex-col h-full items-end justify-end gap-2 w-full sm:w-auto">
+        <div className="flex flex-col items-center w-full">
+          <p className="font-MrsSaintDelafield text-4xl  ">Suivez moi:</p>
+          <div className="flex gap-4 ">
+            <Link href="https://www.instagram.com/bijou.xdesoins/">
+              <Image src={insta} width={30} height={30} alt="logo instagram" />
+            </Link>
+            <Link href="https://www.facebook.com/bijoux.desoins">
+              <Image
+                src={facebook}
+                width={30}
+                height={30}
+                alt="logo facebook"
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="flex w-full sm:w-auto">
+          <p className="w-full sm:w-auto">copyright</p>
+          {isMobile && (
+            <button type="button" onClick={scrollToTop}>
+              <Image src={arrow} width={30} height={30} alt="arrow logo" />
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,39 @@ import photoHome from "../../public/assets/photoHome.png";
 
 function page() {
   return (
-    <div>
-      <Image src={photoHome} width={1400} height={157} alt="photo home page" />
+    <div className="flex flex-col">
+      <div>
+        <Image
+          src={photoHome}
+          width={1600}
+          height={157}
+          alt="photo home page"
+        />
+        <p className="text-gray font-MrsSaintDelafield text-2xl relative bottom-28 left-4 sm:text-8xl sm:relative sm:bottom-80">
+          Institut Bijoux de Soins
+        </p>
+      </div>
+      <div>
+        <h3 className="font-ibarra">L&apos;Institut</h3>
+        <p>Prendre soin de son corps</p>
+        <p>C&apos;est donner envie à son âme d&apos;y rester </p>
+        <p>
+          Bienvenue dans l&apos;univers de Bijoux de Soins. Vous y retrouverez
+          des prestations dédiés à la beauté et au bien-être pour une véritable
+          parenthèse détente. Dans le but de réaliser des soins de beauté
+          relaxants avec des résultats concrets, j&apos;ai choisi des marques
+          françaises respectueuses de l&apos;environnement
+        </p>
+        <p>
+          Ces marques professionnelles proposent des produits de qualités
+          d&apos;une réelle efficacité, composés d&apos;ingrédients
+          d&apos;origine naturelle, conçus, développés et fabriqués
+          exclusivement en France. Des produits cosmétiques Bio tel que le
+          maquillage , les soins pour le visage , le corps et l&apos;hygiène
+          sont disponibles à l&apos;institut pour prendre soin de soi même à la
+          maison .
+        </p>
+      </div>
     </div>
   );
 }
