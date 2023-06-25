e2cca8550bbcdb542ae7ee1089fe4cb4a52ab269
DAMIENDURAND64 <damien64durand@gmail.com>
2023-06-25 22:39:17 +0200

    add comment in beautyTreatmentsPrice

--- app/traitments/beautyTreatmentsPrice/page.tsx
+++ app/traitments/beautyTreatmentsPrice/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 function beautyTreatmentsPrice() {
+  // tu recupere ton id : "efe2bca2-8877-4f60-91d3-0a40ab18514f"
+
+  // tu fait ton getOne avec l'id
+
+  // et ensuite tu peux envoyer ton composant avec les données
   return <div>ici les tarfis des soins</div>;
 }
 
